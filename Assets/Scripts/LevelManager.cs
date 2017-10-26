8f191fa7a68b407046257aea03d5539a665a0e97
michaelkpeterman <michael@36creative.com>
2017-10-26 16:24:33 -0400

    Add basic rack checking

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -6,6 +6,9 @@ public class LevelManager : MonoBehaviour {
 
 	public GameObject cup;
 	private GameObject[] cupStartingPositions;
+
+	List<string> scoredCupPositions = new List<string>();
+
 	private Ball ball;
 	private InGameCanvas inGameCanvas;
 
@@ -42,18 +45,18 @@ public class LevelManager : MonoBehaviour {
 	****************************************************/
 	void PlaceCups() {			
 			
-		GameObject[] currentPositions = GameObject.FindGameObjectsWithTag( "Cup Position" );
-		if ( currentPositions.Length == 5 ) {			
-			foreach( GameObject position in currentPositions ) {
-				// Destroy( position );
-				// TODO: Rr-rack with the appropriate number of cups
-			}
-		} else if ( currentPositions.Length != 5 ) {
-			GameObject[] positions = GameObject.FindGameObjectsWithTag( "Cup Position" );
-			foreach( GameObject position in positions ) {
+		GameObject[] positions = GameObject.FindGameObjectsWithTag( "Cup Position" );
+		foreach( GameObject position in positions ) {
+			if ( ! scoredCupPositions.Contains( position.name ) ) {
 				Instantiate( cup, position.transform.position, Quaternion.identity, position.transform );
-			}	
-		}
+			}
+		}	
+	}
+
+	void RackCups() {		
+		scoredCupPositions.Clear();
+		scoredCupPositions.Add("Cup Position (4)");
+		scoredCupPositions.Add("Cup Position (6)");	
 	}
 
 
@@ -72,19 +75,8 @@ public class LevelManager : MonoBehaviour {
 	****************************************************/
 	public void ResetTable() {			
 		RemoveCups();
+		inGameCanvas.ClearScoreMessage();
 		ball.ResetBall();	
-		PlaceCups();
-		currentGameState = GameState.Ready;
-	}
-
-
-	/***************************************************
-	*** Reset table - with score
-	****************************************************/
-	public void ResetTable( string cupPositionName ) {
-		RemoveCups();
-		ball.ResetBall();	
-		Destroy( GameObject.Find( cupPositionName ) );
 		PlaceCups();
 		currentGameState = GameState.Ready;
 	}
@@ -97,29 +89,12 @@ public class LevelManager : MonoBehaviour {
 		ball.StopBall();
 		inGameCanvas.DisplayScoreMessage();
 		StartCoroutine( ClearScoredCup( cupPositionName ) );
-		StartCoroutine( DelayedResetTable() );
 	}
 
 	IEnumerator ClearScoredCup( string cupPositionName ) {
-		yield return new WaitForSeconds(0.9f);
-		// TODO: use delegation / callback to resolve cup clearing race condition
-		Destroy( GameObject.Find( cupPositionName ) );
+		yield return new WaitForSeconds(1f);
+		RackCups();
+		ResetTable();
 	}
-
-	/***************************************************
-	*** Provide an implementation of the ResetTable
-	*** method specificially for when a player has
-	*** scored a cup
-	****************************************************/
-	IEnumerator DelayedResetTable() {        		
-        yield return new WaitForSeconds(1);
-		RemoveCups();
-		ball.ResetBall();	
-		inGameCanvas.ClearScoreMessage();
-		PlaceCups();
-		currentGameState = GameState.Ready;
-		Debug.Log( GameObject.FindGameObjectsWithTag( "Cup Position" ).Length );
-    }
-
 
 }
